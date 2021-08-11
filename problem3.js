07264d4420de7d5eb98b02e6b06ac7db8edc50ac
amitkumarmountblue <amit.kumar.17@mountblue.tech>
2021-08-11 10:41:03 +0530

    car drill

--- problem3.js
+++ problem3.js
@@ -1,14 +1,30 @@
 function problem3(inventory){
-    for(let i=0;i<inventory.length;i++){
-        for(let j=0;j<inventory.length-1;j++){
-            if(inventory[j].car_model.toLowerCase()>inventory[j+1].car_model.toLowerCase()){
-                let temp=inventory[j+1];
-                inventory[j+1]=inventory[j];
-                inventory[j]=temp;
+    let carModel=[];
+    for(var i=0;i<inventory.length;i++){
+
+        carModel[i]=inventory[i].car_model;
+    }
+    for(let i=0;i<carModel.length;i++){
+        for(let j=i+1;j<carModel.length;j++){
+
+            if(carModel[i]>(carModel[j])){  
+                let temp = carModel[i];  
+                carModel[i] = carModel[j];  
+                carModel[j] = temp;
+            }
+        } 
+    }
+    let result=[]
+    for(let i=0 ; i<carModel.length;i++){
+        for(let j=0;j<inventory.length;j++){
+            if(carModel[i]==(inventory[j].car_model)){
+                result[i]=inventory[j];
+                break;
             }
         }
-        return inventory;
     }
+    return result;
 }
+
 
 module.exports.problem3=problem3;
